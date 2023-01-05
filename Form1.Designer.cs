718bda11e578427098584a059bd4a4be3b16198a
ebecerra00 <106988018+ebecerra00@users.noreply.github.com>
2023-01-05 15:04:27 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace BecerraEstevan_MockLabExam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.lstShow = new System.Windows.Forms.ListBox();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.btnAverage = new System.Windows.Forms.Button();
+            this.btnPrice = new System.Windows.Forms.Button();
+            this.btnSortAlpha = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(17, 15);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(68, 13);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Item\'s Name:";
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Location = new System.Drawing.Point(29, 41);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(56, 13);
+            this.lblPrice.TabIndex = 1;
+            this.lblPrice.Text = "Unit Price:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(91, 12);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(91, 38);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(100, 20);
+            this.txtPrice.TabIndex = 2;
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(102, 64);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(75, 23);
+            this.btnEnter.TabIndex = 3;
+            this.btnEnter.Text = "&Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.BtnEnter_Click);
+            // 
+            // lstShow
+            // 
+            this.lstShow.FormattingEnabled = true;
+            this.lstShow.Location = new System.Drawing.Point(21, 93);
+            this.lstShow.Name = "lstShow";
+            this.lstShow.Size = new System.Drawing.Size(237, 82);
+            this.lstShow.TabIndex = 5;
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Location = new System.Drawing.Point(21, 181);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(75, 23);
+            this.btnDisplay.TabIndex = 4;
+            this.btnDisplay.Text = "&Display";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.BtnDisplay_Click);
+            // 
+            // btnAverage
+            // 
+            this.btnAverage.Location = new System.Drawing.Point(102, 181);
+            this.btnAverage.Name = "btnAverage";
+            this.btnAverage.Size = new System.Drawing.Size(75, 23);
+            this.btnAverage.TabIndex = 5;
+            this.btnAverage.Text = "&Average";
+            this.btnAverage.UseVisualStyleBackColor = true;
+            this.btnAverage.Click += new System.EventHandler(this.BtnAverage_Click);
+            // 
+            // btnPrice
+            // 
+            this.btnPrice.Location = new System.Drawing.Point(183, 181);
+            this.btnPrice.Name = "btnPrice";
+            this.btnPrice.Size = new System.Drawing.Size(75, 23);
+            this.btnPrice.TabIndex = 6;
+            this.btnPrice.Text = "&Price<=5.00";
+            this.btnPrice.UseVisualStyleBackColor = true;
+            this.btnPrice.Click += new System.EventHandler(this.BtnPrice_Click);
+            // 
+            // btnSortAlpha
+            // 
+            this.btnSortAlpha.Location = new System.Drawing.Point(21, 210);
+            this.btnSortAlpha.Name = "btnSortAlpha";
+            this.btnSortAlpha.Size = new System.Drawing.Size(75, 23);
+            this.btnSortAlpha.TabIndex = 7;
+            this.btnSortAlpha.Text = "&Sort Alpha";
+            this.btnSortAlpha.UseVisualStyleBackColor = true;
+            this.btnSortAlpha.Click += new System.EventHandler(this.BtnSortAlpha_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(102, 210);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "&Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(183, 210);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(276, 244);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSortAlpha);
+            this.Controls.Add(this.btnPrice);
+            this.Controls.Add(this.btnAverage);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.lstShow);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.lblName);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.ListBox lstShow;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.Button btnAverage;
+        private System.Windows.Forms.Button btnPrice;
+        private System.Windows.Forms.Button btnSortAlpha;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BecerraEstevan_MockLabExam
+{
+    public partial class Form1 : Form
+    {
+        private const int cSize = 20;
+        private string[] mName = new string [cSize];
+        private double[] mPrice = new double[cSize];
+        private int mIndex = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void DisplayMessageboxOK(string message)
+        {
+            MessageBox.Show(message, Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private bool ValidateInput()
+        {
+            double Price;
+
+            if(txtName.Text == "")
+            {
+                DisplayMessageboxOK("Must enter name");
+                txtName.Focus();
+                return false;
+            }
+
+            if(double.TryParse(txtPrice.Text, out Price)==false)
+            {
+                DisplayMessageboxOK("Price must be a number");
+                txtPrice.Focus();
+                return false;
+            }
+
+            if(Price<=0)
+            {
+                DisplayMessageboxOK("Price must be greater than 0");
+                txtPrice.Focus();
+                return false;
+            }
+
+            return true;
+        }
+
+        private void BtnEnter_Click(object sender, EventArgs e)
+        {
+            string Name;
+            double Price;
+
+            if(ValidateInput() == false)
+            {
+                return;
+            }
+
+            Name = txtName.Text;
+            Price = double.Parse(txtPrice.Text);
+
+            mName[mIndex] = Name;
+            mPrice[mIndex] = Price;
+            mIndex++;
+
+            if(mIndex == cSize)
+            {
+                DisplayMessageboxOK("Array is full");
+                btnEnter.Enabled = false;
+            }
+
+            txtName.Clear();
+            txtPrice.Clear();
+            txtName.Focus();
+        }
+
+        private void BtnDisplay_Click(object sender, EventArgs e)
+        {
+            lstShow.Items.Clear();
+
+            if(mIndex == 0)
+            {
+                DisplayMessageboxOK("Array is empty");
+                return;
+            }
+
+            lstShow.Items.Add("Name" + "\t" + "Price");
+            lstShow.Items.Add("==============");
+
+            for (int k = 0; k < mIndex; k++)
+            {
+                lstShow.Items.Add(mName[k] + "\t" + mPrice[k]);
+            }
+        }
+
+        private void BtnAverage_Click(object sender, EventArgs e)
+        {
+            lstShow.Items.Clear();
+            if(mIndex == 0)
+            {
+                DisplayMessageboxOK("Array is empty");
+                return;
+            }
+
+            double Avg;
+            double Sum = 0;
+
+            for(int k = 0; k<mIndex; k++)
+            {
+                Sum = Sum + +mPrice[k];
+            }
+
+            Avg = Sum / mIndex;
+
+            lstShow.Items.Add("Average: " + Avg.ToString("N2"));
+        }
+
+        private void BtnPrice_Click(object sender, EventArgs e)
+        {
+            lstShow.Items.Clear();
+
+            if(mIndex == 0)
+            {
+                DisplayMessageboxOK("Array is empty");
+                return;
+            }
+
+            bool flag = false;
+
+            lstShow.Items.Add("Name" + "\t" + "Price");
+            lstShow.Items.Add("==============");
+
+            for (int k = 0; k < mIndex; k++)
+            {
+                if(mPrice[k] <= 5.00)
+                {
+                    flag = true;
+                    lstShow.Items.Add(mName[k] + "\t" + mPrice[k]);
+                   
+                }
+
+                //This needs to be out of the for loop
+                if(flag == false)
+                {
+                    DisplayMessageboxOK("There are no items under $5");
+                    lstShow.Items.Clear();
+                }
+
+            }
+
+        }
+
+        private void BtnSortAlpha_Click(object sender, EventArgs e)
+        {
+            lstShow.Items.Clear();
+            if(mIndex == 0)
+            {
+                DisplayMessageboxOK("Array is empty");
+                return;
+            }
+
+            lstShow.Items.Add("Name" + "\t" + "Price");
+            lstShow.Items.Add("==============");
+
+            Array.Sort(mName, mPrice, 0, mIndex);
+
+            for(int k =0; k < mIndex; k++)
+            {
+                lstShow.Items.Add(mName[k] + "\t" + mPrice[k]);
+            }
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            txtName.Clear();
+            txtPrice.Clear();
+            lstShow.Items.Clear();
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
